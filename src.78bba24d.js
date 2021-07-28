84cae8ac73e8c2ff185d87afae39c091272fec2f
PetiurAliona <84270929+PetiurAliona@users.noreply.github.com>
2021-07-28 21:13:34 +0000

    Deploying to gh-pages from @ PetiurAliona/goit-js-hw-11-promisification@1d99698d7e67c0860fe42254ce529b946bb12b41 🚀

---
+++ src.78bba24d.js
@@ -0,0 +1,10 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"LLK9":[function(require,module,exports) {
+const e=e=>{new Promise((o,s)=>o(e),e)},o=e=>console.log(`Resolved after ${e}ms`);e(2e3).then(o);
+},{}],"EEVQ":[function(require,module,exports) {
+const a=[{name:"Mango",active:!0},{name:"Poly",active:!1},{name:"Ajax",active:!0},{name:"Lux",active:!1}],e=(a,e,c)=>{c(a.map(a=>a.name===e?{...a,active:!a.active}:a))},c=a=>console.table(a);
+},{}],"LiEu":[function(require,module,exports) {
+const o=(o,a)=>Math.floor(Math.random()*(a-o+1)+o),a=(a,n,r)=>{const s=o(200,500);setTimeout(()=>{Math.random()>.3?n(a.id,s):r(a.id)},s)},n=(o,a)=>{console.log(`Transaction ${o} processed in ${a}ms`)},r=o=>{console.warn(`Error processing transaction ${o}. Please try again later.`)};
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./js/task-1"),require("./js/task-2"),require("./js/task-3");
+},{"./js/task-1":"LLK9","./js/task-2":"EEVQ","./js/task-3":"LiEu"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.78bba24d.js.map
